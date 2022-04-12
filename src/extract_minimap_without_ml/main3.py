bad3732fd3a75c5d040953cacf5bd516075841c6
curogihu <curoasg@gmail.com>
2022-04-12 21:20:22 +0900

    created to extract minimap without ML

--- src/extract_minimap_without_ml/main3.py
+++ src/extract_minimap_without_ml/main3.py
@@ -20,11 +20,23 @@ for i in range(20):
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result) #類似度が最小,最大となる画素の類似度、位置を調べ代入する
     top_left = max_loc #最も似ている領域を赤で囲う
     bottom_right = (top_left[0] + width, top_left[1] + height) #矩形の右下の座標計算
-    cv2.rectangle(img, top_left, bottom_right, (0, 0, 255), 2) #最も似ている領域を赤の矩形で囲う
+    # cv2.rectangle(img, top_left, bottom_right, (0, 0, 255), 2) #最も似ている領域を赤の矩形で囲う
 
-    # print(top_left, bottom_right)
-    print(base_file_path, top_left, bottom_right, max_val) #最も似ている領域の類似度を表示
-    cv2.imshow("result",img) #別ウィンドウ"result"を開きmgを表示
+    print(top_left, bottom_right)
+    # print(base_file_path, top_left, bottom_right, max_val) #最も似ている領域の類似度を表示
+    # cv2.imshow("result",img) #別ウィンドウ"result"を開きmgを表示
 
-    cv2.waitKey(0) #キー入力待ち
-    cv2.destroyAllWindows() #ウインドウを閉じる
+    # cv2.waitKey(0) #キー入力待ち
+    # cv2.destroyAllWindows() #ウインドウを閉じる
+    x1, y1 = top_left
+    x2, y2 = bottom_right
+
+    print(x1, x2, y1, y2)
+
+
+    extracted_img = img[y1:y2, x1:x2]
+
+    # print(img.shape)
+    # print(extracted_img.shape)
+
+    cv2.imwrite(f'aaaa_{i:04}.jpg', extracted_img)
